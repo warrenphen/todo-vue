01e78b481bf096be098ff6f67b1e4b004165c44e
Warren Phen <warren.phen@gmail.com>
2019-03-16 14:21:56 -0400

    added ToDo List component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <ToDoList/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import ToDoList from './components/ToDoList.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    ToDoList,
   }
 }
 </script>
@@ -24,5 +27,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+* {
+  box-sizing: border-box;
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
 }
 </style>
