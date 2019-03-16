a9c74ac2718a0b96a84cb9ab1982eee75bc8c470
Warren Phen <warren.phen@gmail.com>
2019-03-16 19:08:28 -0400

    created addToDo method

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,14 +1,43 @@
 <template>
   <div class="todolist">
-    <input type="text" class="todo-input" placeholder="What needs to be done?"/>
+    <input type="text" class="todo-input" placeholder="What needs to be done?" v-model="newTodo" @keyup.enter="addToDo"/>
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+        {{ todo.title }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TodoList',
-  props: {
-    msg: String
+  data() {
+      return {
+          newTodo: '',
+          todos: [
+              {
+                  id: 1,
+                  title: 'Finish Screen Cast',
+                  completed: false,
+              },
+              {
+                  id: 2,
+                  title: 'Take Over World',
+                  completed: false,
+              },
+          ]
+      }
+  },
+  methods: {
+      addToDo() {
+        this.todos.push({
+            id: this.idForTodo,
+            title: this.newTodo,
+            completed: false,
+        })
+
+        this.newTodo = ''
+        this.idForTodo++
+      }
   }
 }
 </script>
