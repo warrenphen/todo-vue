5995340d781f0f8f1d03426c5cff8cfb00220b09
Warren Phen <warren.phen@gmail.com>
2019-03-16 20:03:40 -0400

    added ability to edit todo items

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -2,8 +2,9 @@
   <div class="todolist">
     <input type="text" class="todo-input" placeholder="What needs to be done?" v-model="newTodo" @keyup.enter="addToDo"/>
     <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
-        <div>
-            {{ todo.title }}
+        <div class="todo-item-left">
+            <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label">{{ todo.title }}</div>
+            <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" v-focus/>
         </div>
         <div class="remove-item" @click="removeTodo(index)">
             &times;
@@ -24,14 +25,23 @@ export default {
                   id: 1,
                   title: 'Finish Screen Cast',
                   completed: false,
+                  editing: false,
               },
               {
-                  id: 2,
+                  id: 2, 
                   title: 'Take Over World',
                   completed: false,
+                  editing: false,
               },
           ]
       }
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
   },
   methods: {
       addToDo() {
@@ -48,8 +58,14 @@ export default {
         this.newTodo = ''
         this.idForTodo++
       },
-      removeTodo(index){
+      removeTodo(index) {
           this.todos.splice(index, 1)
+      },
+      editTodo(todo) {
+          todo.editing = true;
+      },
+      doneEdit(todo) {
+          todo.editing = false;
       }
   }
 }
@@ -80,6 +96,31 @@ export default {
     margin-left: 14px;
     &:hover {
         color: black;
+    }
+}
+
+.todo-item-left {
+    display: flex;
+    align-items: center;
+}
+
+.todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+}
+
+.todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+    &:focus {
+        outline: none; 
     }
 }
 </style>
